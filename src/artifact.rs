90313eb5cc806bc07d88740bf51f223cfd7085f4
Arthur Cohen <arthur.cohen@embecosm.com>
2022-05-30 15:37:34 +0200

    gh: Fetch artifacts from runs properly

--- src/artifact.rs
+++ src/artifact.rs
@@ -1,16 +1,27 @@
-use octocrab::models::RunId;
-
 // FIXME: Add doc
 // FIXME: Rename
 // FIXME: Probably want that in a method actually
 pub async fn fetch_result_files() {
     let instance = octocrab::instance();
     let actions = instance.actions();
-    let list = actions.list_workflow_run_artifacts("rust-gcc", "testing", RunId(0));
 
-    if let Some(page) = list.send().await.unwrap().value {
-        for artifact in page {
-            dbg!(artifact);
+    // FIXME: No unwrap
+    // We probably need to limit runs which only happened in the last ~90 days.
+    // This should prevent us from running into API rate limits
+    let runs = instance
+        .workflows("rust-gcc", "testing")
+        .list_runs("nightly_run.yml")
+        .send()
+        .await
+        .unwrap();
+
+    for run in runs {
+        let list = actions.list_workflow_run_artifacts("rust-gcc", "testing", run.id);
+        // FIXME: No unwrap
+        if let Some(page) = list.send().await.unwrap().value {
+            for artifact in page {
+                dbg!(artifact);
+            }
         }
     }
 }
