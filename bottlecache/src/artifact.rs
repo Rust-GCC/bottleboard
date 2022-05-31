02bb399cccc8601be92a6a640df47637d11f5787
Arthur Cohen <arthur.cohen@embecosm.com>
2022-05-31 14:23:38 +0200

    cache: Add base for fetching and unzipping result files

--- bottlecache/src/artifact.rs
+++ bottlecache/src/artifact.rs
@@ -1,8 +1,34 @@
+use std::io::{BufReader, Cursor};
+
+// FIXME: Add proper Artifact structure?
+pub struct Artifact(Vec<u8>);
+
+// FIXME: Add documentation
+pub async fn download_artifact(download_url: &str) -> Result<Artifact, reqwest::Error> {
+    let response = reqwest::get(download_url).await?;
+    let zip = response.bytes().await?;
+
+    Ok(Artifact(zip.to_vec()))
+}
+
+pub async fn extract_json(artifact: Artifact) -> Result<(), zip::result::ZipError> {
+    let reader = BufReader::new(Cursor::new(artifact.0));
+    let mut zip = zip::ZipArchive::new(reader)?;
+
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i)?;
+        dbg!(file.name());
+        std::io::copy(&mut file, &mut std::io::stdout())?;
+    }
+
+    todo!()
+}
+
 // FIXME: Add doc
 // FIXME: Rename
 // FIXME: Probably want that in a method actually
 // FIXME: Return the actual files
-pub async fn fetch_result_files() -> Result<(), octocrab::Error> {
+pub async fn fetch_result_files() -> Result<Vec<String>, octocrab::Error> {
     let instance = octocrab::instance();
     let actions = instance.actions();
 
@@ -12,14 +38,18 @@ pub async fn fetch_result_files() -> Result<(), octocrab::Error> {
         .send()
         .await?;
 
+    let mut urls = vec![];
+
     for run in runs {
         let list = actions.list_workflow_run_artifacts("rust-gcc", "testing", run.id);
         if let Some(page) = list.send().await?.value {
             for artifact in page {
-                dbg!(artifact);
+                if artifact.name.ends_with(".json") {
+                    urls.push(artifact.url.into());
+                }
             }
         }
     }
 
-    Ok(())
+    Ok(urls)
 }
--- bottlecache/src/main.rs
+++ bottlecache/src/main.rs
@@ -4,7 +4,12 @@ mod artifact;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    artifact::fetch_result_files().await?;
+    let urls = artifact::fetch_result_files().await?;
+    for url in urls {
+        dbg!(&url);
+        let artifact = artifact::download_artifact(&url).await?;
+        artifact::extract_json(artifact).await?;
+    }
 
     Ok(())
 }
