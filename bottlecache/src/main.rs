890ca1ec7fd6ebcc53d4b9ac8b70ba42484bf34e
Arthur Cohen <arthur.cohen@embecosm.com>
2022-06-07 14:23:55 +0200

    cache: Merge API and cache together

--- bottlecache/src/main.rs
+++ bottlecache/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
-use anyhow::Result;
 use json::TestsuiteResult;
+use rocket::{http::Status, State};
 use structopt::StructOpt;
 
 mod artifact;
@@ -15,8 +15,35 @@ struct Args {
     cache: PathBuf,
 }
 
-async fn fetch_ci_results(args: Args) -> Result<()> {
-    let archives = artifact::fetch_result_files(args.token).await?;
+// FIXME: Move in its own module
+#[derive(Debug)]
+enum Error {
+    GitHub,
+    Unzipping,
+    Disk,
+}
+
+impl From<octocrab::Error> for Error {
+    fn from(_: octocrab::Error) -> Self {
+        Error::GitHub
+    }
+}
+
+impl From<zip::result::ZipError> for Error {
+    fn from(_: zip::result::ZipError) -> Self {
+        Error::Unzipping
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error::Disk
+    }
+}
+
+async fn fetch_ci_results(args: &Args) -> Result<(), Error> {
+    let token = args.token.clone();
+    let archives = artifact::fetch_result_files(token).await?;
     for archive in archives {
         let bytes = artifact::extract_json(archive).await?;
         let json = TestsuiteResult::from_bytes(bytes.as_slice());
@@ -37,9 +64,21 @@ async fn fetch_ci_results(args: Args) -> Result<()> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+#[rocket::get("/")]
+async fn index(state: &State<Args>) -> Status {
+    if let Err(e) = fetch_ci_results(state.inner()).await {
+        dbg!(e);
+        return Status::NoContent;
+    }
+
+    Status::Accepted
+}
+
+#[rocket::launch]
+async fn rocket() -> _ {
     let args = Args::from_args();
 
-    fetch_ci_results(args).await
+    rocket::build()
+        .mount("/", rocket::routes![index])
+        .manage(args)
 }
